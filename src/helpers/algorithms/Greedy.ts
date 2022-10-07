ed5fa69a183ec90c80d63eb283c8a183034d274e
ammar-ahmed22 <ammar-ahmed22@users.noreply.github.com>
2022-10-07 08:17:43 +0000

    Apply formatting changes

--- src/helpers/algorithms/Greedy.ts
+++ src/helpers/algorithms/Greedy.ts
@@ -3,21 +3,21 @@ import { Greedy } from "../../@types/helpers/Node";
 import Node from "../Node";
 import Vec2 from "../Vec2";
 
-export class GreedySolver extends Solver<Greedy>{
-  public initialize({ nodes, start, target, delay }: SolverParams): void {
-    // implement
-  }
+export class GreedySolver extends Solver<Greedy> {
+    public initialize({ nodes, start, target, delay }: SolverParams): void {
+        // implement
+    }
 
-  public getOptimalPath(current: Node<Greedy>): Node<Greedy>[] {
-    const res : Node<Greedy>[] = [];
+    public getOptimalPath(current: Node<Greedy>): Node<Greedy>[] {
+        const res: Node<Greedy>[] = [];
 
-    // implement
+        // implement
 
-    return res
-  }
+        return res;
+    }
 
-  public solve(): Node<Greedy>[] | undefined {
-    // implement
-    return undefined
-  }
+    public solve(): Node<Greedy>[] | undefined {
+        // implement
+        return undefined;
+    }
 }
