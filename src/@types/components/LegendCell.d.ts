3819aa4ef454edeb400af919ddee65061b355995
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2022-10-13 04:02:16 -0400

    added start, target nodes to legend

--- src/@types/components/LegendCell.d.ts
+++ src/@types/components/LegendCell.d.ts
@@ -1,7 +1,10 @@
+import type { IconType } from "react-icons";
+
 export type LegendCellProps = {
     size: string;
     bg?: string | "borderColor";
     bgGradient?: string;
     borderWidth?: string;
     cellName: string;
+    icon?: IconType
 };
